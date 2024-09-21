747a1993c6add1be2e18037f0a87659a595160d8
Frusto <vjbalisnomo@gmail.com>
2024-09-21 02:09:10 -0700

    add vim-better-sml plugin

--- config/plugins/plugins-extra-vim.nix
+++ config/plugins/plugins-extra-vim.nix
@@ -6,6 +6,16 @@
     zen-mode-nvim
     twilight-nvim
     vim-just
+    (vimUtils.buildVimPlugin {
+      name = "vim-better-sml";
+      src = pkgs.fetchFromGitHub {
+        owner = "jez";
+        repo = "vim-better-sml";
+        rev = "a343ee8bed94c2bb35d829f18803bffde8258369";
+        hash = "sha256-LxCo7Q/SbCc+Xib8W5vunOMOoHo1OMrr5crQh81fRss=";
+      };
+    })
+
     {
       plugin = tint-nvim;
       config = "lua require('tint').setup()";
